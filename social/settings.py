72291c4e2671752ac0f3262fb961421312c9c2e8
Kiptoo Victor <victor.kiptoo@student.moringaschool.com>
2021-09-16 00:28:01 +0300

    Deployment

--- social/settings.py
+++ social/settings.py
@@ -55,7 +55,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4oi+4vkvo8cz63!(45f0+pk6os=!i&hgl=hbv^yzdu$rn-q655'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
